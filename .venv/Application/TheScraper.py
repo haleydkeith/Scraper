61f76c8466118bc7b63422cd9e51de91f2e82b8b
Haley D. Keith <64813404+haleydkeith@users.noreply.github.com>
2020-11-02 12:12:39 -0600

    bs4Selectors

--- .venv/Application/TheScraper.py
+++ .venv/Application/TheScraper.py
@@ -2,8 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'http://ethans_fake_twitter_site.surge.sh/'
-response = requests.get (url, timeout=5)
+response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
-print (content)
+tweet = content.find_All('p', attrs={"class": "content"}).text
+# using BS selectors, we want to select pTags, but only ones w/ class name Content
+# .text says that if pTag with class Content found, select text Content of tag
+for tweet in content.find_All('p', attrs={"class": "content"}):
+    print(tweet)
+    
+
+
 
